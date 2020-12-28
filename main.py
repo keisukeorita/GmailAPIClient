42cab3c1291666904a5aa043118bfeb608f897d8
Keisuke Orita <orita.keisuke26@gmail.com>
2020-12-29 01:21:54 +0900

    import static file. Execute modify and delete API when Run button is pressed.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from bottle import route, run, template, redirect, request
+from bottle import route, run, template, redirect, request,get, static_file
 from GmailAPI import GmailAPI
+
+
+# Static file
+@get("/static/css/<filepath:re:.*\.css>")
+def css(filepath):
+    return static_file(filepath, root="static/css")
 
 @route('/main')
 def main():
-    return template("search")
+    return template("main")
 
 @route('/run', method=['GET','POST'])
 def get_massage(): 
@@ -11,21 +17,20 @@ def get_massage():
     mode= request.forms.get("mode")
     DateFrom= request.forms.get("DateFrom")
     DateTo= request.forms.get("DateTo")
-    MessageFrom= request.forms.get("From")
+    MessageFrom= request.forms.get("MessageFrom")
     if mode == "modify":
-        print(mode)
-        print(DateFrom)
-        print(DateTo)
-        print(MessageFrom)
-        # if Gmail.ModifyUnreadMessageList(DateFrom=DateFrom,DateTo=DateTo,MessageFrom=MessageFrom) == False:
-        #    return 'error'
+        if Gmail.ModifyUnreadMessageList(DateFrom=DateFrom,DateTo=DateTo,MessageFrom=MessageFrom) == True:
+            message = "{} email is modified to read".format(Gmail.MessageIDList['resultSizeEstimate'])
+        else:
+            return 'error'
+    elif mode=="delete":
+        if Gmail.DeleteMessageList(DateFrom=DateFrom,DateTo=DateTo,MessageFrom=MessageFrom) == True:
+            message = "{} email is deleted".format(Gmail.MessageIDList['resultSizeEstimate'])
+        else:
+            return 'error'
     else:
-        print(mode)
-        print(DateFrom)
-        print(DateTo)
-        print(MessageFrom)
-        # if Gmail.DeleteMessageList(DateFrom=DateFrom,DateTo=DateTo,MessageFrom=MessageFrom) == False:
-        #    return 'error'
-    return template("result")
+        return 'error'
+
+    return message
 
 run(host='localhost', port=8080)
